ceec2c7cba67cd63e6c32f7bd5ddb0ff6d6fa3c8
Dhruvil Shah <dhruvilshah@Dhruvils-MBP.attlocal.net>
2024-08-07 17:42:08 -0400

    fix the object-store

--- iceberg-rust/src/io/object_store.rs
+++ iceberg-rust/src/io/object_store.rs
@@ -4,15 +4,16 @@ use datafusion::error::DataFusionError;
 use object_store::ObjectStore;
 use object_store::aws::AmazonS3Builder;
 use aws_sdk_s3::Client;
+use object_store::local::LocalFileSystem;
 
 pub fn get_object_store(
-    url: &Url,
+    url: &str,
     region: Option<&str>,
 ) -> Result<Arc<dyn ObjectStore>, DataFusionError> {
-    let url_str = url.as_str();
     let region = region.unwrap_or("us-east-1");
 
-    if url_str.starts_with("s3://") {
+    if url.starts_with("s3://") {
+        let mut url = Url::parse(url).unwrap();
         if let Some(bucket_name) = url.host_str() {
             let store = Arc::new(
                 AmazonS3Builder::from_env()
@@ -22,7 +23,8 @@ pub fn get_object_store(
             );
             return Ok(store);
         }
-    } else if url_str.starts_with("oss://") {
+    } else if url.starts_with("oss://") {
+        let mut url = Url::parse(url).unwrap();
         if let Some(bucket_name) = url.host_str() {
             let store = Arc::new(
                 AmazonS3Builder::from_env()
@@ -32,6 +34,12 @@ pub fn get_object_store(
             );
             return Ok(store);
         }
+    } else {
+        let store: Arc<dyn ObjectStore> =
+            Arc::new(LocalFileSystem::new_with_prefix(url).unwrap());
+
+        return Ok(store);
+
     }
 
     Err(DataFusionError::Execution(format!(
